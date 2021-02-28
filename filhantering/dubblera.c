61b7cf37d20370bbcdbdc75b3f944d86ded6aac5
Jeanette Heidenberg <jeanette.heidenberg@ha.ax>
2021-02-28 21:37:47 +0200

    Lade till freopen stderr för windows.

--- filhantering/dubblera.c
+++ filhantering/dubblera.c
@@ -3,6 +3,8 @@
 int main(){
     int single;
 
+    // freopen("errlogg.txt","a", stderr);
+
     while(scanf("%d", &single) == 1){
         printf("%d\n", single * 2);
         fprintf(stderr, "Läste %d, beräknade %d\n", single, single*2);
