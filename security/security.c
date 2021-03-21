debb951ed1ca71a127a1d044d44dd7187174c32a
Jeanette Heidenberg <jeanette.heidenberg@ha.ax>
2021-03-21 17:33:51 +0200

    Exempel säkerhetsrisker

---
+++ security/security.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void vulnerable_print(char * malicious_input) {
+	printf(malicious_input);
+}
+
+int main () {
+    char buffer[10];
+    char * message = "Bra jobbat!";
+
+    printf("Vad vill du skriva ut >");
+    gets(buffer); // Vad händer om användaren skriver mer än 10 tecken?
+
+    vulnerable_print(buffer);  // Vad händer om användaren matade in en formatsträng?
+
+    printf("%s\n", message);
+}
