47fe2e3fb0e4776cdd08244a144a99add14332d6
Jeanette Heidenberg <jeanette.heidenberg@ha.ax>
2022-01-23 16:36:40 +0200

    Added more preprocessor examples.

---
+++ preprocessorn/compare2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#ifdef DEBUG_ON
+#define DEBUG(msg) printf("DEBUG: %s\n", msg)
+#else
+#define DEBUG(msg)
+#endif
+
+int main(int argc, char * argv[]){
+    DEBUG("Program started");
+    printf("Hello world!\n");
+}
